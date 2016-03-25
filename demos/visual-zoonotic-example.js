75cbee4acdddd5570383ce24508c08a609a26eaa
BZKFTemple <wzkflynn@gmail.com>
2016-03-25 16:32:11 -0400

    another example

--- demos/visual-zoonotic-example.js
+++ demos/visual-zoonotic-example.js
@@ -4,7 +4,10 @@ let options;
 let environment;
 let step = 0.01;
 let agents = [];
-let actions, states, conditions, transitions, SIRModel;
+let livestock = [];
+let water;
+let pathogen;
+let actions, states, conditions, transitions, SIRModel, SIRLivestockModel;
 let seed = 0x12345678;
 let random = new Random(Random.engines.mt19937().seedWithArray([seed, 0x90abcdef]));
 
@@ -31,16 +34,28 @@ let margin = {
   height = 250 - margin.top - margin.bottom;
 
 function init(options) {
-  let bounds = [300, 90]; // for margin
+  let bounds = [300, 100]; // for margin
   let boundaries = {
-    "cows": {top:50, left:10, bottom:10, right:100}
+    "livestock": {
+      top: 50,
+      bottom: 10,
+      left: 10,
+      right: 70
+    },
+    "people": {
+      top: 140,
+      bottom: 50,
+      left: 150,
+      right: 300
+    }
   }
+  let numLivestock = 50;
   let numAgents = options.numberOfAgents;
   let infectedAtStart = options.infectedAtStart;
   raycaster.far = options.shedRange;
   step = options.step;
 
-  let pathogen = options.pathogen;
+  pathogen = options.pathogen;
   pathogen.decayRate = 200;
   pathogen['beta-Poisson'] = function(dose) {
     let response = 1 - Math.pow((1 + (dose / pathogen.N50) * (Math.pow(2, (1 / pathogen.optParam)) - 1)), (-pathogen.optParam));
@@ -50,6 +65,21 @@ function init(options) {
     let response = 1 - Math.exp(-pathogen.optParam * dose);
     return response;
   }
+
+  water = {
+    width: 80,
+    length: 80,
+    depth: 10,
+    pathConc: 0
+  }
+
+  water.mesh = new THREE.Mesh(new THREE.PlaneGeometry(water.width, water.length), new THREE.MeshBasicMaterial({
+    color: 0x5599ff
+  })),
+  water.volume = water.width * water.length * water.depth,
+  water.mesh.position.x = 110;
+  water.mesh.position.y = 60;
+  scene.add(water.mesh);
 
   for (let i = 0; i < numAgents; i++) {
     let mesh = new THREE.Mesh(new THREE.TetrahedronGeometry(1, 1), new THREE.MeshBasicMaterial({
@@ -65,24 +95,54 @@ function init(options) {
       },
       prevX: 0,
       prevY: 0,
+
       timeInfectious: 0,
       timeRecovered: 0,
       mesh: mesh,
-      objectives: [],
-      pastObjectives: []
+      waterAvailable: 100,
+      waterPathConcentration: 0,
+      dailyWaterRequired: 3000,
+      boundaryGroup: 'people'
     };
     agents[i].physContact = -0.0135 * (Math.pow(agents[i].age - 43, 2)) + 8;
     agents[i].mesh.qId = i;
     agents[i].mesh.type = 'agent';
-    agents[i].mesh.position.x = random.real(0, 1) * bounds[0];
-    agents[i].mesh.position.y = random.real(0, 1) * bounds[1];
+    agents[i].mesh.position.x = random.real(boundaries.people.left, boundaries.people.right);
+    agents[i].mesh.position.y = random.real(boundaries.people.bottom, boundaries.people.top);
     scene.add(agents[i].mesh);
   }
 
+  for (var j = 0; j < numLivestock; j++){
+    livestock[j] = {
+      id : j + numAgents,
+      age : 5,
+      prevX: 0,
+      prevY: 0,
+      mesh : new THREE.Mesh(new THREE.CubeGeometry(2, 1, 0.5), new THREE.MeshBasicMaterial({color: 0xcc44cc})),
+      pathogenLoad: 0,
+      timeInfectious: 0,
+      timeRecovered: 0,
+      waterAvailable: 100,
+      waterPathConcentration: 0,
+      dailyWaterRequired: 3000,
+      gPerDayExcrete: 4000,
+      states: {
+        illness: 'succeptible'
+      },
+      boundaryGroup: 'livestock'
+    }
+    livestock[j].physContact = numLivestock;
+    livestock[j].mesh.qId = j + numAgents;
+    livestock[j].mesh.position.x = random.real(boundaries.livestock.left, boundaries.livestock.right);
+    livestock[j].mesh.position.y = random.real(boundaries.livestock.bottom, boundaries.livestock.top);
+    livestock[j].mesh.type = 'agent';
+    scene.add(livestock[j].mesh);
+  }
+
   for (var r = 0; r < infectedAtStart; r++) {
-    var rIndex = Math.floor(numAgents * random.real(0, 1));
-    agents[rIndex].states.illness = 'infectious';
-    agents[rIndex].pathogenLoad = 1e4;
+    var rIndex = Math.floor(numLivestock * random.real(0, 1));
+    livestock[rIndex].states.illness = 'infectious';
+    livestock[rIndex].pathogenLoad = 1e4;
   }
 
   //some actions are shared across states.
@@ -95,9 +155,9 @@ function init(options) {
         let intersects = raycaster.intersectObjects(scene.children);
         intersects.forEach(function(d) {
           if (d.object.type === 'agent') {
-            let contactedAgent = agents[d.object.qId];
+            let contactedAgent = environment.agents[environment._agentIndex[d.object.qId]];
             if (contactedAgent.states.illness === 'succeptible') {
-              contactedAgent.pathogenLoad += jStat.normal.inv(random.real(0,1),pathogen.shedRate * step,pathogen.shedRate *step);
+              contactedAgent.pathogenLoad += jStat.normal.inv(random.real(0, 1), pathogen.shedRate * step, pathogen.shedRate * step);
               contactedAgent.lastInfectedContact = agent.id;
               contactedAgent.responseProb = pathogen[pathogen.bestFitModel](contactedAgent.pathogenLoad);
             }
@@ -105,28 +165,49 @@ function init(options) {
         });
       }
     },
-    move: function(step, agent) {
-      var maxDistPerDay = 500;
-      var ageM = maxDistPerDay - (Math.abs(43 - agent.age) / 43 * maxDistPerDay) + 3e-4;
-      var x = random.real(-1, 1) * ageM + (agent.prevX * 0.98);
-      var y = random.real(-1, 1) * ageM + (agent.prevY * 0.98);
-      agent.mesh.position.y += x * step;
-      agent.mesh.position.x += y * step;
-      agent.mesh.rotation.z = Math.atan2(x * step, y * step);
-      agent.prevX = x;
-      agent.prevY = y;
+    excrete: function(step, agent){
+      water.pathConc += agent.gPerDayExcrete * step / (water.volume * 1000);
     },
-    moveWithin:function(step, agent){
+    checkWater: function(step, agent) {
+      if (agent.waterAvailable < 1) {
+        actions.getWater(step, agent);
+      } else {
+        actions.drink(step, agent);
+      }
+    },
+    drink: function(step, agent) {
+      agent.waterAvailable -= agent.dailyWaterRequired * step;
+      agent.pathogenLoad += agent.waterPathConcentration * step;
+    },
+    getWater: function(step, agent) {
+      agent.waterAvailable += agent.dailyWaterRequired * 0.333; //needs to get water 3 times a day
+      agent.waterPathConcentration += water.pathConc * (agent.waterAvailable / 1000);
+    },
+    moveWithin: function(step, agent) {
+      var boundry = boundaries[agent.boundaryGroup];
       var maxDistPerDay = 500;
-      var ageM = maxDistPerDay - (Math.abs(43 - agent.age) / 43 * maxDistPerDay) + 3e-4;
-      var x = random.real(-1, 1) * ageM + (agent.prevX * 0.98);
-      var y = random.real(-1, 1) * ageM + (agent.prevY * 0.98);
-      if(boundaries[])
-      agent.mesh.position.y += x * step;
-      agent.mesh.position.x += y * step;
-      agent.mesh.rotation.z = Math.atan2(x * step, y * step);
-      agent.prevX = x;
-      agent.prevY = y;
+      var individualRate = maxDistPerDay - (Math.abs(43 - agent.age) / 43 * maxDistPerDay) + 3e-4;
+      var dx = step * (random.real(-1, 1) * individualRate + (agent.prevX * 0.98));
+      var dy = step * (random.real(-1, 1) * individualRate + (agent.prevY * 0.98));
+      var nextX = agent.mesh.position.x + dx;
+      var nextY = agent.mesh.position.y + dy;
+      if (nextX > boundry.right) {
+        dx = 0;
+      }
+      if (nextX < boundry.left){
+        dx = 0;
+      }
+      if (nextY > boundry.top ) {
+        dy = 0;
+      }
+      if (nextY < boundry.bottom){
+        dy = 0;
+      }
+        agent.mesh.position.x += dx;
+        agent.mesh.position.y += dy;
+        agent.mesh.rotation.z = Math.atan2(dx, dy);
+        agent.prevX = dx / step;
+        agent.prevY = dy / step;
     }
   };
 
@@ -136,7 +217,8 @@ function init(options) {
       agent.mesh.material.color.set(0x00ff00);
       agent.timeRecovered = 0;
       agent.timeInfectious = 0;
-      actions.move(step, agent);
+      actions.checkWater(step, agent);
+      actions.moveWithin(step, agent);
       if (agent.pathogenLoad > 0) {
         agent.responseProb = pathogen[pathogen.bestFitModel](agent.pathogenLoad);
         agent.pathogenLoad -= pathogen.decayRate * Math.log(agent.pathogenLoad) * step;;
@@ -148,13 +230,18 @@ function init(options) {
     'infectious': function(step, agent) {
       infectious++;
       agent.mesh.material.color.set(0xff0000);
-      actions.move(step, agent);
+      actions.moveWithin(step, agent);
+      actions.checkWater(step, agent);
       actions.contact(step, agent);
+      if(agent.boundaryGroup === 'livestock'){
+        actions.excrete(step, agent);
+      }
       agent.timeInfectious += 1 * step;
     },
     'removed': function(step, agent) {
       agent.mesh.material.color.set(0x0000ff);
-      actions.move(step, agent);
+      actions.moveWithin(step, agent);
+      actions.checkWater(step, agent);
       if (agent.pathogenLoad > 2) {
         agent.pathogenLoad -= pathogen.decayRate * Math.log(agent.pathogenLoad) * step;
       } else {
@@ -202,16 +289,18 @@ function init(options) {
   }];
 
   SIRModel = new QEpiKit.StateMachine('sir-model', states, transitions, conditions, agents);
+  SIRLivestockModel = new QEpiKit.StateMachine('sir-livestock-model', states, transitions, conditions, livestock);
 
   //the environmental class can takes resources, facilities, and events as its first three arguements. Here we have none. We've also set the agent activation to 'random'.
   environment = new QEpiKit.Environment([], [], [], 'random', function() {
     return random.real(0, 1);
   });
   environment.add(SIRModel);
+  environment.add(SIRLivestockModel);
   environment.run(step, step * 2, 0);
 
   //live update
-  camera.position.z = 80;
+  camera.position.z = 120;
   camera.position.x = bounds[0] * 0.5;
   camera.position.y = bounds[1] * 0.5;
   camera.rotation.x = 8 * Math.PI / 180;
@@ -247,7 +336,6 @@ function init(options) {
   yAxis = d3.svg.axis()
     .scale(y)
     .orient("left");
-
   render();
 }
 
@@ -255,6 +343,9 @@ function render() {
   if (environment.time <= duration) {
     infectious = 0;
     requestAnimationFrame(render);
+    water.mesh.material.color.r = water.pathConc / pathogen.N50;
+    water.mesh.material.color.g = water.pathConc / pathogen.N50;
+    water.mesh.material.color.b = 1 - water.pathConc / pathogen.N50;
     environment.update(step);
     environment.time += step;
     renderer.render(scene, camera);
